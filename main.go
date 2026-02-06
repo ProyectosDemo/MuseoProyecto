f3ea4a476641d87b3e48d9daa7be4c5a124299d1
Demo <demolinkle@gmail.com>
2026-02-05 22:46:06 -0400

    si

--- main.go
+++ main.go
@@ -4,12 +4,7 @@ import
 
 func main() {
 	// jose estuvo aqui
-<<<<<<< HEAD
-	// comentario
-	coco()
-=======
 	grapql.Coco()
->>>>>>> 9c719f6548c1f5215988a52a21953ac4ab0a05cd
 	/*
 		// ----------------------  main ----------------------
 		router := gin.Default()
