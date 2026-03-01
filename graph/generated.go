595b1e16738cafe23f98d372b460156a4631713e
Demo <demolinkle@gmail.com>
2026-03-01 10:46:58 -0400

    crud cliente y trabajador frontend

--- graph/generated.go
+++ graph/generated.go
@@ -117,6 +117,7 @@ type ComplexityRoot struct {
 		LoginCliente    func(childComplexity int, email string, password string) int
 		LoginTrabajador func(childComplexity int, login string, password string) int
 		Obras           func(childComplexity int, limit *int32, offset *int32) int
+		Trabajadores    func(childComplexity int, limit *int32, offset *int32) int
 	}
 
 	Trabajador struct {
@@ -147,6 +148,7 @@ type MutationResolver interface {
 }
 type QueryResolver interface {
 	Clientes(ctx context.Context, limit *int32, offset *int32) ([]*model.Cliente, error)
+	Trabajadores(ctx context.Context, limit *int32, offset *int32) ([]*model.Trabajador, error)
 	Obras(ctx context.Context, limit *int32, offset *int32) ([]*model.Obra, error)
 	FindCliente(ctx context.Context, id string) ([]*model.Cliente, error)
 	FindTrabajador(ctx context.Context, id string) ([]*model.Trabajador, error)
@@ -653,6 +655,17 @@ func (e *executableSchema) Complexity(ctx context.Context, typeName, field strin
 		}
 
 		return e.ComplexityRoot.Query.Obras(childComplexity, args["limit"].(*int32), args["offset"].(*int32)), true
+	case "Query.Trabajadores":
+		if e.ComplexityRoot.Query.Trabajadores == nil {
+			break
+		}
+
+		args, err := ec.field_Query_Trabajadores_args(ctx, rawArgs)
+		if err != nil {
+			return 0, false
+		}
+
+		return e.ComplexityRoot.Query.Trabajadores(childComplexity, args["limit"].(*int32), args["offset"].(*int32)), true
 
 	case "Trabajador.admin":
 		if e.ComplexityRoot.Trabajador.Admin == nil {
@@ -979,6 +992,22 @@ func (ec *executionContext) field_Query_Clientes_args(ctx context.Context, rawAr
 }
 
 func (ec *executionContext) field_Query_Obras_args(ctx context.Context, rawArgs map[string]any) (map[string]any, error) {
+	var err error
+	args := map[string]any{}
+	arg0, err := graphql.ProcessArgField(ctx, rawArgs, "limit", ec.unmarshalOInt2ᚖint32)
+	if err != nil {
+		return nil, err
+	}
+	args["limit"] = arg0
+	arg1, err := graphql.ProcessArgField(ctx, rawArgs, "offset", ec.unmarshalOInt2ᚖint32)
+	if err != nil {
+		return nil, err
+	}
+	args["offset"] = arg1
+	return args, nil
+}
+
+func (ec *executionContext) field_Query_Trabajadores_args(ctx context.Context, rawArgs map[string]any) (map[string]any, error) {
 	var err error
 	args := map[string]any{}
 	arg0, err := graphql.ProcessArgField(ctx, rawArgs, "limit", ec.unmarshalOInt2ᚖint32)
@@ -2997,6 +3026,59 @@ func (ec *executionContext) fieldContext_Query_Clientes(ctx context.Context, fie
 	}()
 	ctx = graphql.WithFieldContext(ctx, fc)
 	if fc.Args, err = ec.field_Query_Clientes_args(ctx, field.ArgumentMap(ec.Variables)); err != nil {
+		ec.Error(ctx, err)
+		return fc, err
+	}
+	return fc, nil
+}
+
+func (ec *executionContext) _Query_Trabajadores(ctx context.Context, field graphql.CollectedField) (ret graphql.Marshaler) {
+	return graphql.ResolveField(
+		ctx,
+		ec.OperationContext,
+		field,
+		ec.fieldContext_Query_Trabajadores,
+		func(ctx context.Context) (any, error) {
+			fc := graphql.GetFieldContext(ctx)
+			return ec.Resolvers.Query().Trabajadores(ctx, fc.Args["limit"].(*int32), fc.Args["offset"].(*int32))
+		},
+		nil,
+		ec.marshalNTrabajador2ᚕᚖmainᚋgraphᚋmodelᚐTrabajadorᚄ,
+		true,
+		true,
+	)
+}
+
+func (ec *executionContext) fieldContext_Query_Trabajadores(ctx context.Context, field graphql.CollectedField) (fc *graphql.FieldContext, err error) {
+	fc = &graphql.FieldContext{
+		Object:     "Query",
+		Field:      field,
+		IsMethod:   true,
+		IsResolver: true,
+		Child: func(ctx context.Context, field graphql.CollectedField) (*graphql.FieldContext, error) {
+			switch field.Name {
+			case "id":
+				return ec.fieldContext_Trabajador_id(ctx, field)
+			case "nombre":
+				return ec.fieldContext_Trabajador_nombre(ctx, field)
+			case "login":
+				return ec.fieldContext_Trabajador_login(ctx, field)
+			case "password":
+				return ec.fieldContext_Trabajador_password(ctx, field)
+			case "admin":
+				return ec.fieldContext_Trabajador_admin(ctx, field)
+			}
+			return nil, fmt.Errorf("no field named %q was found under type Trabajador", field.Name)
+		},
+	}
+	defer func() {
+		if r := recover(); r != nil {
+			err = ec.Recover(ctx, r)
+			ec.Error(ctx, err)
+		}
+	}()
+	ctx = graphql.WithFieldContext(ctx, fc)
+	if fc.Args, err = ec.field_Query_Trabajadores_args(ctx, field.ArgumentMap(ec.Variables)); err != nil {
 		ec.Error(ctx, err)
 		return fc, err
 	}
@@ -6311,6 +6393,28 @@ func (ec *executionContext) _Query(ctx context.Context, sel ast.SelectionSet) gr
 					}
 				}()
 				res = ec._Query_Clientes(ctx, field)
+				if res == graphql.Null {
+					atomic.AddUint32(&fs.Invalids, 1)
+				}
+				return res
+			}
+
+			rrm := func(ctx context.Context) graphql.Marshaler {
+				return ec.OperationContext.RootResolverMiddleware(ctx,
+					func(ctx context.Context) graphql.Marshaler { return innerFunc(ctx, out) })
+			}
+
+			out.Concurrently(i, func(ctx context.Context) graphql.Marshaler { return rrm(innerCtx) })
+		case "Trabajadores":
+			field := field
+
+			innerFunc := func(ctx context.Context, fs *graphql.FieldSet) (res graphql.Marshaler) {
+				defer func() {
+					if r := recover(); r != nil {
+						ec.Error(ctx, ec.Recover(ctx, r))
+					}
+				}()
+				res = ec._Query_Trabajadores(ctx, field)
 				if res == graphql.Null {
 					atomic.AddUint32(&fs.Invalids, 1)
 				}
